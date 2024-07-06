9dac976d6a826e684bde5d42b713ab023cf39ab5
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-06 18:26:48 -0500

    se corrigio el codigo y ahora funciona correctamente

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,25 +1,20 @@
 #include<iostream>
 using namespace std;
 int main (){
-    int n,v,h;
-    int par[50];
-    int impar[50];
+    int n;
     cout<<"ingresé el la cantidad de celdas: ";cin>>n;
     int numero[n];
+    int par[n];
+    int impar[n];
     for(int i =0;i<n;i++){
         cout<<"ingresé el valor: ";cin>>numero[i];
         if(numero[i]%2==0){
-            h++;
-            número[i]=par[h];
-        }else 
-            v++;
-            numero[i]=impar[v];
+                par[i]=numero[i];
+                cout<<"el número de par es: "<< par[i] <<endl;
+        }else{
+                impar[i]=numero[i];
+                 cout<<"el número impar es: "<< impar[i] <<endl;
+        }
     }
-    for(int j =0;j<h;j++){
-        cout<<"el número de par es: "<<par[h]<<endl;
-    }
-    for(int b=0;b<v;b++){
-        cout<<"el número impar es: "<<impar[b]<<endl;
-    }
-    
+    return 0;
 }
