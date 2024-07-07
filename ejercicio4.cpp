1ed1da42b4705527934172ecd0a4b04ad788a73a
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-07 10:55:02 -0500

    se esta pensando alguna solucion para el codigo

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
+void generar(int );
 int main(){
-    int a,b;
+    int a;
     cout<<"ingrese la cantidad de elementos que tendra el arreglo: ";cin>>a;
-    int num[a];
-    
-
+    generar(a);
+    return 0;
 }
+void generar(int a){
+    int r=0;
+    int num[a];
+    cout<<"ingrese el primer digito a almacenar: ";cin>>num[0];
+    for(int i=1;i<a;i++){
+        r=r+num[0];
+        r=num[i];
+        cout<<"los numeros generados son: "<<num[i]<<endl;
+    }
+} 
