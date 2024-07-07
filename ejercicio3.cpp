374569d17dac7b83cfcba6734268a926258b2229
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-07 12:20:38 -0500

    se realizo el ejercicio 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int m=100,r,s,t,h;
+    int m=100,r,s,t,h,g=0;
     int temperatura[7];
     int temmin[7];
     int temmax[7];
@@ -18,15 +18,18 @@ int main(){
         for(int b=0;b<1;b++){
             cout<<"ingrese la temperatura mayor: ";cin>>temmax[b];
             t=temmax[b];
-                    if(temmax[b]==temmax[b]){
-                        h++;
-                        
-                    }
         }
         r=(s+t)/2;
         cout<<"la temperatura media fue: "<<r<<endl;
     }
     cout<<"la temperatura menor en toda la semana fue: "<<m<<endl;
+    cout<<"ingrese la temperatura: ";cin>>h;
+    for(int i=0;i<7;i++){
+        if(temmax[i]==h){
+            g++;
+        }
+    }
+    cout<<"los dias que coincidieron con la temperatura ingresada fue: "<<g<<endl;
 
     return 0;
 }
