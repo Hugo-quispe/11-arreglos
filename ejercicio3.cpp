f79e87cfdd8716ba6b058ec2f48d058f1498164b
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-07 10:06:47 -0500

    se corrigio el valor dado a la temperatura minima y se le asigno la variable m cun valor de 100 para que asi tengo una temperatura y asi se pueda modificar cada que se ingrese una temperatura menor

---
+++ ejercicio3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m=100,r,s,t,h;
+    int temperatura[7];
+    int temmin[7];
+    int temmax[7];
+    int n;
+    for(int i=0;i<7;i++){
+        cout<<"registro de temperatura minima y temperatura maxima. Dia:"<<i+1<<endl;
+        for(int j=0;j<1;j++){
+            cout<<"ingrese la temperatura min: ";cin>>temmin[j];
+            if(temmin[j]<m){
+                m=temmin[j];
+            }
+            s=temmin[j];            
+        }
+        for(int b=0;b<1;b++){
+            cout<<"ingrese la temperatura mayor: ";cin>>temmax[b];
+            t=temmax[b];
+                    if(temmax[b]==temmax[b]){
+                        h++;
+                        
+                    }
+        }
+        r=(s+t)/2;
+        cout<<"el promedio de la temperatura es: "<<r<<endl;
+    }
+    cout<<"la temperatura menor fue: "<<m<<endl;
+
+    return 0;
+}
