7305f29a69f6ee195ab44fabb39b4c635046e16f
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-07 11:36:48 -0500

    por alguna razon al querer imprimir el nombre del estudiante en ambas asignaturas y usar un estructura for se repite indefinidamente

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -3,13 +3,26 @@ using namespace std;
 int main (){
     int a,b;
     cout<<"ingrese la cantidad de estudiantes de fundamentos de programacion: ";cin>>a;
-    cout<<"ingrese la cantidad de estudiantes de programacion grafica: ";cin>>b;
-    char fp[a];
+    string fp[a];
     for(int i=0;i<a;i++){
         cout<<"ingrese el nombre del estudiante: ";cin>>fp[i];
     }
-    char pg[b];
-    for(int j=0;j<b;j++){
-        cout<<"ingrese el nombre del estudiante: ";cin>>pg[j];
+    cout<<"ingrese la cantidad de estudiantes de programacion grafica: ";cin>>b;
+    string pg[b];
+    for(int i=0;i<b;i++){
+        cout<<"ingrese el nombre del estudiante: ";cin>>pg[i];
     }
+    int p=0;
+    string comun[999];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<a;j++){
+            if(fp[i]==pg[j]){
+                comun[p]=fp[i];
+                    p++;
+            }
+        }
+    }
+    cout<<"el total de estudiantes en comun son: "<<p<<endl;
+   
+    return 0;
 }
